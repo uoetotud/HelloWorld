e202df721df9a7288921cfe23f91796b3e2e0234
uoetotud <uoetotud@gmail.com>
2015-03-22 20:58:49 +0100

    first&second commits in branch

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Rossi!");
+		
+		System.out.println("First commit.");
+
+		System.out.println("Edit from bash and commit.");
 	}
 
 }
